0c9edda6e98a83d397099a4e23f9172e09008237
cvhf <c.vhf@fu-berlin.de>
2013-12-10 21:35:16 +0100

    Added about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,19 @@ describe "StaticPages" do
         expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help Page" do
+      it "should have the content 'Help'" do
+          visit '/static_pages/help'
+          expect(page).to have_content('Help')
+      end
+  end
+
+  describe "About Page" do
+      it "should have the content 'About Us'" do
+          visit '/static_pages/about'
+          expect(page).to have_content('About Us')
+      end
+  end
 end
+
